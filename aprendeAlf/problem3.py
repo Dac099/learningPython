ca2cfb32d089b97fcd6c8ac0c2d5cba08cb93e01
David Ceja Zapata <davidcejazapata@gmail.com>
2021-05-03 22:52:34 -0500

    c

--- aprendeAlf/problem3.py
+++ aprendeAlf/problem3.py
@@ -11,18 +11,18 @@ while True:
     >> """)
     if opc == '1':
         # Preguntar los datos del cliente, crear un diccionario con los datos y añadirlo a la base de datos.
-        datos_cliente = dict()
-        datos_cliente['Nombre'] = input('Ingrese el nombre del cliente: ')
-        datos_cliente['Direccion'] = input('Ingrese la direccion del cliente: ')
-        datos_cliente['Telefono'] = input('Ingrese el telefono del cliente: ')
-        datos_cliente['Correo'] = input('Ingrese el correo del cliente: ')
+        nif_cliente = input('Ingrese la NIF del cliente [NIF-Letra]: ')
+        base_de_datos[nif_cliente] = dict()
+        base_de_datos[nif_cliente]['Nombre'] = input('Ingrese el nombre del cliente: ')
+        base_de_datos[nif_cliente]['Direccion'] = input('Ingrese la direccion del cliente: ')
+        base_de_datos[nif_cliente]['Telefono'] = input('Ingrese el telefono del cliente: ')
+        base_de_datos[nif_cliente]['Correo'] = input('Ingrese el correo del cliente: ')
         preferencia = input('El cliente es preferente? [y/n]: ')
         if preferencia == 'y': 
-            datos_cliente['Preferente'] = True  
+            base_de_datos[nif_cliente]['Preferente'] = True  
         else:
-            datos_cliente['Preferente'] = False
-        nif_cliente = input('Ingrese la NIF del cliente [NIF-Letra]: ')
-        base_de_datos[nif_cliente] = datos_cliente
+            base_de_datos[nif_cliente]['Preferente'] = False
+        print(f'Cliente {nif_cliente} agregado')
         continue
     elif opc == '2':
         # Preguntar por el NIF del cliente y eliminar sus datos de la base de datos.
@@ -35,16 +35,22 @@ while True:
         nif_cliente = input('Ingrese la NIF del cliente [NIF-Letra]: ')
         if nif_cliente in base_de_datos:
             for key in base_de_datos[nif_cliente]:
-                print(key,'\t',base_de_datos[nif_cliente[key]])
+                print(f'{key}   {base_de_datos[nif_cliente][key]}')
         else: 
             print(f'Cliente {nif_cliente} no encontrado')
-            
+        continue
     elif opc == '4':
         # Mostrar lista de todos los clientes de la base datos con su NIF y nombre.
-        pass
+        for indice in base_de_datos:
+            for key in base_de_datos[indice]:
+                print(f'{key}   {base_de_datos[indice][key]}')
+        continue
     elif opc == '5':
         # Mostrar la lista de clientes preferentes de la base de datos con su NIF y nombre.
-        pass
+        for indice in base_de_datos:
+            if base_de_datos[indice]['Preferente'] == True:
+                print(base_de_datos[indice]['Nombre'],'\t', indice)
+        continue
     elif opc == '6':
         # Terminar el programa.
         break
